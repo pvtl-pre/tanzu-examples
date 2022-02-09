211eac7e654332a659bb95e0b9488a4461dcf761
Pre Bhakta <pbhakta@vmware.com>
2022-02-09 11:15:22 -0600

    Removing getting unused az client id

--- tap/1.0.x/scripts/01-prep-azure-objects.sh
+++ tap/1.0.x/scripts/01-prep-azure-objects.sh
@@ -5,19 +5,6 @@ shopt -s nocasematch;
 TKG_LAB_SCRIPTS="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source "$TKG_LAB_SCRIPTS/set-env.sh"
 
-#
-# Setup and preflight checks
-#
-
-# Get client id
-AZURE_CREDENTIALS_CONFIGURED=false
-export AZURE_CLIENT_ID=$(yq e .azure.client-id "$PARAMS_YAML")
-# NOTE: yq returns null string...
-if [ "$AZURE_CLIENT_ID" != "null" ]; then
-  echo "INFO: client id already configured in $PARAMS_YAML, not setting up azure credentials"
-  AZURE_CREDENTIALS_CONFIGURED=true
-fi
-
 # Ensure az is configured and working
 if ! az account show > /dev/null; then
   echo "ERROR: could not run az account show, please configure az"
